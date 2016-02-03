8dd7cf039af8dc279aac9304c4da84c336ee5d50
JOSHUA ARNOLD <joshuaarnold@arnoldjr.local>
2016-02-02 23:25:51 -0500

    Bokeh plot added

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template, request, redirect
 
+from bokeh.resources import CDN 
+from bokeh.util.string import encode_utf8
+
+from bokeh_maker import make_plot
+ 
 app = Flask(__name__)
+app.vars = {'plot_name':'cmg',
+    'bokeh_script':'',
+    'bokeh_div':''}
+
+flag,script,div = make_plot(app.vars['plot_name'])
+if flag:
+  app.vars['bokeh_script'] = script
+  app.vars['bokeh_div'] = div
 
 @app.route('/')
 def main():
@@ -8,8 +21,19 @@ def main():
 
 @app.route('/index')
 def index():
-  return render_template('carto.html')
+  js_resources = CDN.render_js()
+  css_resources = CDN.render_css()
+
+  html =  render_template(
+      'index.html',
+      js_resources=js_resources,
+      css_resources=css_resources,
+      bokeh_script=app.vars['bokeh_script'],
+      bokeh_div=app.vars['bokeh_div'],
+      )
+  return encode_utf8(html)
+
 
 if __name__ == '__main__':
-  #app.run(port=33507)
-  app.run(host='0.0.0.0',debug=False)
+  app.run(debug=True)
+  #app.run(host='0.0.0.0',debug=False)
