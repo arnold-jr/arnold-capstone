857c8a153896f95f549c80ff9f45b6f46a45e09f
arnold-jr <j.arnold.111@gmail.com>
2016-03-14 22:08:44 -0400

    bulletChart included

--- app.py
+++ app.py
@@ -51,17 +51,18 @@ except:
   print "Database read unsuccessfully"
   pass
 
-# Read in pickled sklearn model and store as app member 
-def model_reader():
-  with open('./data/p_combo_model_rf.dpkl', 'rb') as p_input:
-    model = pickle.load(p_input)
-  app.vars['model'] = model
+if False:
+  # Read in pickled sklearn model and store as app member 
+  def model_reader():
+    with open('./data/p_combo_model_rf.dpkl', 'rb') as p_input:
+      model = pickle.load(p_input)
+    app.vars['model'] = model
 
-try:
-  model_reader()
-except:
-  print "Model read unsuccessfully"
-  pass
+  try:
+    model_reader()
+  except:
+    print "Model read unsuccessfully"
+    pass
 
 
 def prep_plot_df(q_address):
@@ -106,30 +107,61 @@ def request_cartodb():
 
 @app.route('/_run_address')
 def run_address():
-  q_address = request.args.get('a', 'No Address Specified', type=str)
-
-  address, zipcode = prep_plot_df(q_address) 
-
-  if zipcode != -42:
-    df = app.vars['this_df']
-    df = df.loc[:,['SALEPRICE']+
-        [c for c in df.columns.tolist() if ("_count" in c)]]
-
-    df['SALEPRICE'] = df['SALEPRICE']/10000.0
-
-    chart_data_json = []
-    for i,row in df.iterrows():  
-      val_list = [
-          {'label':DISPLAY_NAMES[k.replace("_count","")], 'value':v} 
-          for k,v in row.iteritems()]
-      chart_data_json.append({'key': i, 'values': val_list}) 
-
-    return jsonify(result=address,
-        result_df=chart_data_json)
+  '''Prepares the data to plot by querying the database and returns JSON
+  to the d3 javascript'''
   
-  else:
+  df = app.vars['df'].copy()
+  df['SALEPRICE'] = df['SALEPRICE']/100000.0
+
+  
+  q_address = request.args.get('a', 'No Address Specified', type=str)
+  
+  # geocode the address and convert to lat_lng 
+  lat_lng, zipcode, address = get_zip_and_lat_lng(
+      google_geocode_api_key,q_address)
+  zipcode = int(zipcode)
+  
+  if zipcode not in df.zipcode.unique():
     return jsonify(result=address,
-        result_df=[])
+        result_df=[],
+        result_saleprice=[])
+  
+  lat, lng = (float(s) for s in lat_lng.split(","))
+
+  # Use nearest neighbor as prediction
+  idx_min = (np.square(df['longitude'].astype(np.float64) - lng)
+      +np.square(df['latitude'].astype(np.float64) - lat)).idxmin()
+  # Ensure that we only get one row of the dataframe
+  query_df = df.loc[[idx_min],:].mean().to_frame(address[:10]).transpose()
+  
+  # combine zip and query dataframes
+  this_df = (df.groupby('zipcode').mean().loc[zipcode,:].to_frame()
+      .transpose())
+  this_df = query_df.append(this_df)
+
+  this_df = this_df.loc[:,['SALEPRICE']+
+      [c for c in this_df.columns.tolist() if ("_count" in c)]]
+
+  zip_samples = df.loc[df.zipcode==zipcode,:].copy()
+  ranges = [zip_samples.SALEPRICE.min(), zip_samples.SALEPRICE.mean(), 
+    zip_samples.SALEPRICE.max()]
+  saleprice_json = {
+    "title":"Last sale price / $100k",      
+    "subtitle":"",    
+    "ranges":ranges,  
+    "measures":[this_df.SALEPRICE[0]]
+  }
+  this_df.drop('SALEPRICE',axis=1,inplace=True)
+  chart_data_json = []
+  for i,row in this_df.iterrows():  
+    val_list = [
+        {'label':DISPLAY_NAMES[k.replace("_count","")], 'value':v} 
+        for k,v in row.iteritems()]
+    chart_data_json.append({'key': i, 'values': val_list}) 
+
+  return jsonify(result=address,
+      result_df=chart_data_json,
+      result_saleprice=saleprice_json)
 
 
 @app.route('/', methods=['POST', 'GET'])
--- static/multiBar.js
+++ static/multiBar.js
@@ -1,76 +1,88 @@
-    var long_short_data = [
-        {
-            key: "Zipcode",
-            values: [
-                {
-                    "label" : "Group A" ,
-                    "value" : -1.8746444827653
-                } ,
-                {
-                    "label" : "Group B" ,
-                    "value" : -8.0961543492239
-                } ,
-                {
-                    "label" : "Group C" ,
-                    "value" : -0.57072943117674
-                } ,
-                {
-                    "label" : "Group D" ,
-                    "value" : -2.4174010336624
-                } ,
-                {
-                    "label" : "Group E" ,
-                    "value" : -0.72009071426284
-                } ,
-                {
-                    "label" : "Group F" ,
-                    "value" : -2.77154485523777
-                } ,
-                {
-                    "label" : "Group G" ,
-                    "value" : -9.90152097798131
-                } ,
-                {
-                    "label" : "Group H" ,
-                    "value" : 14.91445417330854
-                } ,
-                {
-                    "label" : "Group I" ,
-                    "value" : -3.055746319141851
-                }
-            ]
-        },
-    ];
+$(function() {
+var submit_form = function(e) {
+  $.getJSON($SCRIPT_ROOT + '/_run_address', {
+    a: $('input[name="a"]').val()
+  }, function(data) {
+    $('input[name=a]').focus().select();
+    $('#get_address_result').text(data.result);
+  
+    histWrapper(data.result_df);
+    bulletWrapper(data.result_saleprice);
 
-    var llong_short_data = $("#result").data
-
-    var chart;
-    nv.addGraph(function() {
-        chart = nv.models.multiBarHorizontalChart()
-            .x(function(d) { return d.label })
-            .y(function(d) { return d.value })
-            //.yErr(function(d) { return [-Math.abs(d.value * Math.random() * 0.3), Math.abs(d.value * Math.random() * 0.3)] })
-            .barColor(d3.scale.category20().range())
-            .duration(250)
-            .margin({left: 100})
-            .stacked(true);
-
-        chart.yAxis.tickFormat(d3.format(',.2f'));
-
-        chart.yAxis.axisLabel('Count or Price / $100,000');
-        chart.xAxis.axisLabel('Amenity Type').axisLabelDistance(20);
-
-        d3.select('#chart1 svg')
-            .datum(long_short_data)
-            .call(chart);
-
-        nv.utils.windowResize(chart.update);
-
-        chart.dispatch.on('stateChange', function(e) { nv.log('New State:', JSON.stringify(e)); });
-        chart.state.dispatch.on('change', function(state){
-            nv.log('state', JSON.stringify(state));
-        });
-        return chart;
-    });
+  });
+  return false;
+};
+$('a#calculate').bind('click', submit_form);
+$('input[type=text]').bind('keydown', function(e) {
+  if (e.keyCode == 13) {
+    submit_form(e);
+  }
+});
+$('input[name=a]').focus();
 
 
+var bulletWrapper = function(new_data) {
+  var chart;
+  d3.select("#chart").selectAll("svg > *").remove()
+  nv.addGraph(function() {
+      chart = nv.models.bulletChart()
+          .margin({left: 150});
+
+      d3.select('#chart svg')
+          .datum(new_data)
+          .call(chart);
+
+      nv.utils.windowResize(chart.update);
+
+      chart.dispatch.on('stateChange', function(e) { 
+          nv.log('New State:', JSON.stringify(e)); });
+      chart.state.dispatch.on('change', function(state){
+          nv.log('state', JSON.stringify(state));
+      });
+      return chart;
+  });
+};
+
+var histWrapper = function(new_data) {
+  var chart;
+  d3.select("#chart1").selectAll("svg > *").remove()
+  nv.addGraph(function() {
+      chart = nv.models.multiBarHorizontalChart()
+          .x(function(d) { return d.label })
+          .y(function(d) { return d.value })
+          .barColor(d3.scale.category20().range())
+          .duration(500)
+          .margin({left: 150})
+          .stacked(false)
+          .showControls(false);
+
+      chart.yAxis.tickFormat(d3.format(',.2f'));
+
+      chart.yAxis.axisLabel('Count or Price / $10,000');
+
+      d3.select('#chart1 svg')
+          .datum(new_data)
+          .call(chart);
+
+      nv.utils.windowResize(chart.update);
+
+      chart.dispatch.on('stateChange', function(e) { 
+          nv.log('New State:', JSON.stringify(e)); });
+      chart.state.dispatch.on('change', function(state){
+          nv.log('state', JSON.stringify(state));
+      });
+      return chart;
+  });
+
+};
+
+function exampleData() {
+  return {
+    "title":"Revenue",      //Label the bullet chart
+    "subtitle":"US$, in ten thousands",     //sub-label for bullet chart
+    "ranges":[150,225,300],  //Minimum, mean and maximum values.
+    "measures":[220]        //Value representing current measurement (the thick blue line in the example)
+  };
+}
+
+});
